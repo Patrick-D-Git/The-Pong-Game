733fb02edc24f394336424196a874bcde2517aae
Patrick D <patrick.montejo@gmail.com>
2024-02-24 11:44:58 -0500

    added a visual line in the middle

--- scoreboard.py
+++ scoreboard.py
@@ -6,14 +6,19 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.color("white")
-        self.penup()
         self.hideturtle()
+        self.penup()
         self.l_score = 0
         self.r_score = 0
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
+        self.goto(0, 300)
+        self.pendown()
+        self.goto(0, -300)
+        self.penup()
+
         self.goto(-100, 200)
         self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
         self.goto(100, 200)
